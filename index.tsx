f7481081ee6263b9b2b9eecdcca2e108a46894d7
Compleksity <codercompleksity@gmail.com>
2026-09-03 17:02:35 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,16 @@
+import { CopyIdIcon } from "@components/Icons";
+import { copyToClipboard } from "@utils/clipboard";
+import definePlugin from "@utils/types";
 
+export default definePlugin({
+    name: "CopyUserID",
+    description: "Adds a button to copy the user ID of a message author.",
+    authors: [],
+    messagePopoverButton: {
+        label: "Copy User ID",
+        icon: CopyIdIcon,
+        onClick: message => {
+            copyToClipboard(message.author.id);
+        }
+    }
+});
